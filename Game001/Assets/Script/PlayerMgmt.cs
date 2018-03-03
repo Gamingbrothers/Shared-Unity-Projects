6aa1fd26415e9c8ed865d2e586f46fa1d642bf7e
miwi1972 <mikael.winander@hotmail.com>
2018-03-03 20:48:44 +0100

    Pathfinidng scripts

---
+++ Game001/Assets/Script/PlayerMgmt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMgmt : MonoBehaviour {
+
+    public GameObject seeker;
+
+    void OnMouseDown()
+    {
+        GameObject unit = (GameObject)Instantiate(seeker, transform.position, Quaternion.identity); 
+    }
+
+}
