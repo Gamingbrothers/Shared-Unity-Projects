37cae17fb40a7c1453f7a379661b0ecc2f591eae
miwi1972 <mikael.winander@hotmail.com>
2018-03-03 23:16:07 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/Gamingbrothers/Shared-Unity-Projects""
    This reverts commit 6e03597c3ac1c0c237c970c10901a964a8273179.

---
+++ Game001/Assets/Script/GameControll/Script_GameMaster.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_GameMaster : MonoBehaviour {
+
+    //Variables 
+    #region
+    public int playerOneHealth = 100;
+    public int playerOneMoney = 0;
+    public int playerOneMoneyGet = 1;
+
+    public int playerTwoHealth = 100;
+    public int playerTwoMoney = 0;
+    public int playerTwoMoneyGet = 1; 
+
+    //for UI
+    public Text playerOneMoneyText;
+    public Slider playerOneHPSlider; 
+
+    
+
+
+    #endregion
+
+
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("GenerteMoneyPlayerOne", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerOneHPSlider.value = playerOneHealth;
+        playerOneMoneyText.text = playerOneMoney.ToString(); 
+
+	}
+
+    public void GenerteMoneyPlayerOne()
+    {
+        playerOneMoney = playerOneMoney + playerOneMoneyGet;
+    }
+
+}
